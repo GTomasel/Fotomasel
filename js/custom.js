a7320b87f6ac48206c6b85a4b19c1d96ef7dd92a
Gabriel Tomasel <gabitomasel@gmail.com>
2021-08-07 15:22:05 -0300

    Version Final

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 //Slick Custom
-
 $('.youtube').slick({
     arrows: true,
     dots: true,
@@ -23,11 +22,8 @@ $('.youtube').slick({
 
 //Boton subir a top
 var botonVolver = document.getElementById("botonVolver");
-
 var rootElement = document.documentElement;
-
 window.onscroll = function() {scrollFunction()};
-
 function scrollToTop() {
   rootElement.scrollTo({
     top: 0,
@@ -36,19 +32,13 @@ function scrollToTop() {
 }
 
 botonVolver.addEventListener("click", scrollToTop);
-
 var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-
 var rootElement = document.documentElement
-
 function handleScroll() {
-  // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
   if ((rootElement.scrollTop / scrollTotal ) > 0.95 ) {
-    // Show button
     botonVolver.classList.add("flechaVolver--show")
   } else {
-    // Hide button
     botonVolver.classList.remove("flechaVolver--show")
   }
 }
@@ -58,10 +48,4 @@ document.addEventListener("scroll", handleScroll)
 //Collapse navbar on click
 $('.js-scroll-trigger').click(function() {
   $('.navbar-collapse').collapse('hide');
-});
-
-$(document).ready(function(){
-  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
-  });
 });
